4bef1f54711ab6724df7cf5b8f0e08f6467f6994
openshift-ops <bishesh.halloween959@gmail.com>
2022-06-02 01:46:32 +0530

    simple gradle project

---
+++ springboot-java/src/main/java/com/example/test/springbootjava/SpringbootJavaApplication.java
@@ -0,0 +1,20 @@
+package com.example.test.springbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@SpringBootApplication
+public class SpringbootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaApplication.class, args);
+
+
+		}
+
+}
